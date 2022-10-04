37af7154a5612370e0a9d43fe83fae23d6f437f7
Rune Nilsen <runen@egdeconsulting.no>
2022-10-04 19:02:50 +0200

    init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using Models;
+
+Model1 model = new Model1()
+{
+    Description = "descriptionstring"
+};
+Console.WriteLine(model.Description);
+Console.WriteLine(model.External);
---
+++ ConsoleApp1/Models/Model1.cs
@@ -0,0 +1,7 @@
+namespace Models;
+
+public class Model1
+{
+    public string Description { get; set; }
+    public string External = "external";
+}
